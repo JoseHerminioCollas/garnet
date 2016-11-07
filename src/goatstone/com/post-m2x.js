3402c63835425ec431cb6f99f8658f4c6ca22b18
joseherminiocollas <joseherminiocollas@goatstone.com>
2016-11-07 12:50:43 -0800

    Some modification.

--- src/goatstone/com/post-m2x.js
+++ src/goatstone/com/post-m2x.js
@@ -1,23 +1,12 @@
 var https = require('https')
+var config = require('./config')
 
-var h = 'api-m2x.att.com'
-var p = '/v2/devices/20e971a5aae0e7aeeec434356221fcf1/updates/'
-var options2 = {
-    hostname: h,
-    port: 443,
-    path: p,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'X-M2X-KEY' : '3ef381bb98c492d4a6b21c363eb236d1'
-    }
-}
 function callM2X(lightLevels){
     var pack = {"values":{
         "light": lightLevels
     }}
     var jsonP = JSON.stringify(pack)
-    var req = https.request(options2, function(res) {
+    var req = https.request(config, function(res) {
         console.log('STATUS: ${res.statusCode}', res.statusCode );
         res.setEncoding('utf8');
         res.on('data', function(chunk) {

e0b74f2e27a3006dcf9368aeabf1df757576d864
joseherminiocollas <joseherminiocollas@goatstone.com>
2016-11-15 12:36:06 -0800

    Edit stub, add test to M2X tests.

--- test/goatstone.com.m2x.test.js
+++ test/goatstone.com.m2x.test.js
@@ -5,34 +5,41 @@ var M2X = require('goatstone/com/m2x.js')
 
 var m2x = new M2X()
 var expectedData = {"mock":true}
+var httpsStub
 
-before(function(){
-    sinon.stub(https, "request",
-        function(conf, cb){
-            cb({
-                setEncoding:function(){},
-                on:function(e, cb){
-                    if(e==='data'){
-                        cb(expectedData)
-                    }
-                }
-            })
-            return {
-                on:function(e, cb ){
-                    cb('chunk') },
-                write:function(){},
-                end:function(){}}
-            })
-})
-after(function(){
-    console.log('after')//
-    https.request.restore()
-})
 describe('M2X', function() {
+    before(function(){
+        httpsStub = sinon.stub(https, "request",
+            function(conf, cb){
+                cb({
+                    setEncoding:function(){},
+                    on:function(e, cb){
+                        if(e==='data'){
+                            cb(expectedData)
+                        }
+                    }
+                })
+                return {
+                    on:function(e, cb ){
+                        cb('chunk') },
+                    write:function(){},
+                    end:function(){}}
+            })
+    })
+    after(function(){
+        httpsStub.restore()
+    })
+
     describe('Call to backend', function() {
         it('should return a JSON object', function( done ) {
             m2x.post().then(function(x){
                 expect(x).to.equal(expectedData)
+                done()
+            })
+        });
+        it('should be called with a correct configuration object', function( done ) {
+            m2x.post().then(function(){
+                expect(httpsStub.getCall(0).args[0].hostname).to.equal('api-m2x.att.com')
                 done()
             })
         });
